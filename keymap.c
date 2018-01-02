82a3a55df48019475bc841f2e2cf2ac5f7dabe4c
Rafael Delboni <rafadelboni@gmail.com>
2018-01-02 17:36:42 +0100

    feat: printscreen and caps simplification

--- keymap.c
+++ keymap.c
@@ -2,7 +2,6 @@
 #include "timer.h"
 
 enum {
-  MR_ESC_CAPS = 0,
   MR_EMO_SHRG = 1,
   MR_EMO_TFLP = 2
 };
@@ -13,7 +12,6 @@ enum {
 
 // Buttons
 #define _______ KC_TRNS
-#define ESC_CAPS M(MR_ESC_CAPS)
 #define EM_SHRG M(MR_EMO_SHRG)
 #define EM_TFLP M(MR_EMO_TFLP)
 
@@ -24,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
    * |----------------------------------------------------------------|
-   * |ESC/CAP|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
@@ -34,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BL] = KEYMAP_ANSI(
   KC_GESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  ESC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
+  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
   KC_LSFT,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,KC_UP,KC_PGDN, \
   KC_LCTL,  KC_LGUI,KC_LALT,                KC_SPC,                        KC_RALT,MO(_FL),KC_RCTRL, KC_LEFT,KC_DOWN,KC_RGHT),
 
@@ -42,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |   |   |   |   |   |     |Hme |
+   * |     |   |Up |   |   |   |   |   |   |   |   |   |   |PScr |Hme |
    * |----------------------------------------------------------------|
    * |      |<- |Dn | ->|   |   |   |   |TRP|TR-|TR+|   |        |End |
    * |----------------------------------------------------------------|
@@ -53,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = KEYMAP_ANSI(
   _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
-  _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______, _______,KC_HOME, \
+  _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______, KC_PSCR,KC_HOME, \
   _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______,KC_MPLY,KC_MPRV,KC_MNXT,_______,        _______,KC_END, \
   _______,EM_SHRG,EM_TFLP,BL_DEC, BL_TOGG,BL_INC, _______,KC_MUTE,KC_VOLD,KC_VOLU,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
   _______,_______,_______,                 _______,               _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
@@ -64,29 +62,9 @@ void tap(uint16_t keycode){
   unregister_code(keycode);
 };
 
-bool is_caps_on = false;
-
 // Macro Definitions
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
   switch (id) {
-    case MR_ESC_CAPS:
-      // Shift + Caps for Capslock
-      if (keyboard_report->mods & MOD_BIT(KC_LSFT)) {
-        if(is_caps_on) {
-          // Turn capslock on
-          is_caps_on = false;
-          return MACRO( U(CAPS), END );
-        } else {
-          // Turn capslock off
-          is_caps_on = true;
-          return MACRO( D(CAPS), END );
-        }
-      } else {
-        if (record->event.pressed) {
-          return MACRO( T(ESC), END);
-        }
-      }
-      break;
     case MR_EMO_TFLP: // (╯°□°)╯ ︵ ┻━┻
       if(record->event.pressed){
         register_code(KC_RSFT);
